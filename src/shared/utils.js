ea62fa1a49cdfea123a643ae23ed5bbf2883c2b2
Wakushi <mzozord@gmail.com>
2023-07-31 09:16:21 +0200

    Optimized getHousingRatingStars()

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,14 +1,13 @@
 function getHousingRatingStars(housingRating) {
-	let stars = []
-	for (let i = 0; i < housingRating; i++) {
-		stars.push(<i key={i + "a"} className="fa-solid fa-star rated"></i>)
-	}
-	if (housingRating < 5) {
-		for (let i = 0; i < 5 - housingRating; i++) {
-			stars.push(<i key={i + "b"} className="fa-solid fa-star"></i>)
-		}
-	}
-	return stars
+	return Array.from({ length: 5 }, (_, i) => {
+		return (
+			<i
+				className={`fa-solid fa-star ${
+					i < housingRating ? "rated " : ""
+				}`}
+			></i>
+		)
+	})
 }
 
 export { getHousingRatingStars }
